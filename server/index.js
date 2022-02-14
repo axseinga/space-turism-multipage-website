cb6b992310aa6d685cb5e88455b01d73edb571ce
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2022-02-14 20:41:49 +0000

    add all GET routes

--- server/index.js
+++ server/index.js
@@ -5,17 +5,45 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-const destinations = await readFile(
-    new URL("data/destination.json", import.meta.url),
-    "utf-8"
+const destinations = JSON.parse(
+    await readFile(new URL("data/destination.json", import.meta.url), "utf-8")
 );
 
-app.get("/api/v1/destinations", (req, res) => {
+const crew = JSON.parse(
+    await readFile(new URL("data/crew.json", import.meta.url), "utf-8")
+);
+
+const technologies = JSON.parse(
+    await readFile(new URL("data/technology.json", import.meta.url), "utf-8")
+);
+
+const getDestinations = (req, res) => {
     res.status(200).json({
         status: "success",
+        results: destinations.length,
         data: { destinations: destinations },
     });
-});
+};
+
+const getCrew = (req, res) => {
+    res.status(200).json({
+        status: "success",
+        results: crew.length,
+        data: { crew: crew },
+    });
+};
+
+const getTechnologies = (req, res) => {
+    res.status(200).json({
+        status: "success",
+        results: technologies.length,
+        data: { technologies: technologies },
+    });
+};
+
+app.route("/api/v1/destinations").get(getDestinations);
+app.route("/api/v1/crew").get(getCrew);
+app.route("/api/v1/technologies").get(getTechnologies);
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
