db0c1cc5225ac707b6c06d0abfe9edc608a4d28e
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2022-02-16 17:53:52 +0000

    move server to separate file

--- server/index.js
+++ server/index.js
@@ -3,14 +3,10 @@ import { router as destinationRouter } from "./routes/destinationRoutes.js";
 import { router as crewRouter } from "./routes/crewRoutes.js";
 import { router as technologyRouter } from "./routes/technologyRoutes.js";
 
-const PORT = process.env.PORT || 3001;
-
 const app = express();
 
 app.use("/api/v1/destinations", destinationRouter);
 app.use("/api/v1/crew", crewRouter);
 app.use("/api/v1/technologies", technologyRouter);
 
-app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-});
+export default app;
