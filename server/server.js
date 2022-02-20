8e79d83178c4514f1627230c8dad4d4cfb1c8136
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2022-02-20 14:12:54 +0000

    connected mongoDB

--- server/server.js
+++ server/server.js
@@ -1,6 +1,16 @@
 import app from "./index.js";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 dotenv.config();
+
+const DB = process.env.DATABASE.replace(
+    "<PASSWORD>",
+    process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB, { useNewUrlParser: true }, () =>
+    console.log("connected to db!")
+);
 
 const PORT = process.env.PORT || 3001;
 
