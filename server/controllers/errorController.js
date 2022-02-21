301fb0879ddb6512d312aabef79756d532cef93c
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2022-02-21 15:03:48 +0000

    add custom error handling to operational errors and split error message for dev and prod

--- server/controllers/errorController.js
+++ server/controllers/errorController.js
@@ -1,9 +1,35 @@
+const sendErrorDev = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+        error: err,
+        stack: err.stack,
+    });
+};
+
+const sendErrorProd = (err, res) => {
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message,
+        });
+    } else {
+        console.err("ERROR!", err);
+
+        res.status(500).json({
+            status: "error",
+            message: "Something went wrong.",
+        });
+    }
+};
+
 export default (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || "error";
 
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message,
-    });
+    if (process.env.NODE_ENV === "development") {
+        sendErrorDev(err, res);
+    } else if (process.env.NODE_ENV === "production") {
+        sendErrorProd(err, res);
+    }
 };
