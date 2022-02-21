da3ffc976016f33779bbab56df4cbef9e9ca7456
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2022-02-21 14:03:09 +0000

    add appError function and errorController to handle errors

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import { router as destinationRouter } from "./routes/destinationRoutes.js";
 import { router as crewRouter } from "./routes/crewRoutes.js";
 import { router as technologyRouter } from "./routes/technologyRoutes.js";
+import { AppError } from "./utils/appError.js";
+import globalErrorHandler from "./controllers/errorController.js";
 
 const app = express();
 app.use(express.json());
@@ -9,5 +11,11 @@ app.use(express.json());
 app.use("/api/v1/destinations", destinationRouter);
 app.use("/api/v1/crew", crewRouter);
 app.use("/api/v1/technologies", technologyRouter);
+
+app.all("*", (req, res, next) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server.`, 404));
+});
+
+app.use(globalErrorHandler);
 
 export default app;
