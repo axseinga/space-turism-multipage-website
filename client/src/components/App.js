cca01e1656a1e771657e8f141b3b9f56c6231152
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2022-02-23 12:25:01 +0000

    add global style and normalize

---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+import { GlobalStyle } from '../styles/globalStyle';
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <div className="App">hello world</div>
+    </>
+  );
+};
+
+export default App;
